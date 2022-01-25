f333fb51fbfd419ebae3bd48308346b7d5087906
isomoth <ic.gonzalez35@hotmail.com>
2022-01-25 18:31:55 +0100

    Fix: Test fetch URL in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ export const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<Home />}></Route>
-          <Route path='/foods/iron' element={<FoodList />}></Route>
+          <Route path='/foods/' element={<FoodList />}></Route>
           <Route path='/tracker' element={<FoodTracker />}></Route>
         </Routes>
       </Router>
