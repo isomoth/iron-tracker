4f2a50759a7479277209837551a1bd855a2be02b
isomoth <ic.gonzalez35@hotmail.com>
2022-01-25 16:50:28 +0100

    Fix: Update fetch URL and map through correct items

--- frontend/src/components/FoodList.js
+++ frontend/src/components/FoodList.js
@@ -5,20 +5,20 @@ export const FoodList = () => {
   const [foods, setFoods] = useState([]);
 
   useEffect(() => {
-    fetch(API_URL('foods'))
+    fetch(API_URL('foods?iron=60'))
       .then((res) => res.json())
-      .then((data) => setFoods(data.food));
-  });
+      .then((data) => setFoods(data));
+  }, []);
 
   return (
     <section>
       <h1>TOP IRON-RICH FOODS</h1>
+      <h2>(mg per 100g serving)</h2>
       {foods.map((food) => (
-        <div key={foods._id}>
-          <div>
-            <h3>Food: {foods.food}</h3>
-            <p>Iron: {foods.iron}</p>
-          </div>
+        <div key={food._id}>
+          <h3>{food.food}</h3>
+          <p>Iron: {food.iron}</p>
+          <p>Vitamin C: {food.vitamin_c}</p>
         </div>
       ))}
     </section>
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,3 @@
 const BASE_URL = 'https://iron-tracker.herokuapp.com';
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
-
-/* export const API_URL = (_id) =>
-  `https://iron-tracker.herokuapp.com/foods/id/${_id}`;
- */
